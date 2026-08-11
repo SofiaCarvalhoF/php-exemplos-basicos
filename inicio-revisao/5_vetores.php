31a5b16debe12d7a516b0f036d6e970995227af3
SofiaCarvalhoF <sofcf7587@gmail.com>
2026-08-11 08:21:37 -0300

    Exemplo 5

--- inicio-revisao/5_vetores.php
+++ inicio-revisao/5_vetores.php
@@ -1,19 +1,19 @@
 <?php
 
 // Vetor (array simples - "1 linha")
-$frutas = ["Maça", "Banana", "Uva"];
+$frutas = ["Maçã", "Banana", "Uva"];
 
 //Exibindo os valores com laço
 foreach ($frutas as $indice => $fruta) {
-    echo "Posição $indice: $fruta <br>";"
+    echo "Posição $indice: $fruta <br>";
 }
 
     //Array (Linhas e colunas) com pilotos da F1
-
+ 
     $matriz = [
     ["Max Verstappen", "Oscar Piastri", "Lando Norris"],
     ["Charles Leclerc", "Lewis Hamilton", "George Russell"],
-    ["Fernando Alonso", "Carlos Sainz", "Gabriel Bortoleto"],  
+    ["Fernando Alonso", "Carlos Sainz", "Gabriel Bortoleto"]  
 ];
 
 echo "<br><br>";
@@ -23,7 +23,7 @@ echo "Lista de Pilotos de F1 (Dica: O 1º é o melhor.)
 // Exibindo os valores da matriz
 foreach ($matriz as $linha) {
     foreach ($linha as $piloto) {
-        echo $piloto ." | ";"
+        echo $piloto . " | ";
 }
 echo "<br>";
 }
